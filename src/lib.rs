de92f9118be1d035c931812c3a5df838473205ec
Philipp Rehner <prehner@ethz.ch>
2021-11-05 14:52:06 +0100

    add gc-pcsaft equation of state

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use feos_core::python::{PyInit_cubic, PyInit_user_defined};
 use feos_dft::python::PyInit_feos_dft;
+use feos_gc_pcsaft::python::PyInit_feos_gc_pcsaft;
 use pyo3::prelude::*;
 use pyo3::wrap_pymodule;
 use quantity::python::PyInit_quantity;
@@ -13,6 +14,8 @@ pub fn feos(py: Python<'_>, m: &PyModule) -> PyResult<()> {
 
     m.add_wrapped(wrap_pymodule!(feos_dft))?;
 
+    m.add_wrapped(wrap_pymodule!(feos_gc_pcsaft))?;
+
     py.run(
         "\
 import sys
@@ -22,6 +25,9 @@ sys.modules['feos.user_defined'] = user_defined
 sys.modules['feos.cubic'] = cubic
 
 sys.modules['feos.fmt'] = feos_dft
+
+sys.modules['feos.gc_pcsaft'] = feos_gc_pcsaft
+sys.modules['feos.gc_pcsaft.eos'] = feos_gc_pcsaft.eos
     ",
         None,
         Some(m.dict()),
