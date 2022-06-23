321976eed19e61a20feb4528a28cf9a6d4d549ba
Gernot Bauer <gernotbauer85@googlemail.com>
2022-06-23 14:43:48 +0200

    Add introduction sentence and example for feos crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! FeOs - An open-source framework for equations of state and classical functional density theory.
+//!
+//! # Example: critical point of a pure substance
+//!
+//! ```rust
+//! # use feos_core::EosError;
+//! use feos::pcsaft::{PcSaft, PcSaftParameters};
+//! use feos_core::parameter::{IdentifierOption, Parameter};
+//! use feos_core::{Contributions, State};
+//! use quantity::si::KELVIN;
+//! use std::rc::Rc;
+//! 
+//! // Read parameters from json file.
+//! let parameters = PcSaftParameters::from_json(
+//!     vec!["propane"],
+//!     "tests/pcsaft/test_parameters.json",
+//!     None,
+//!     IdentifierOption::Name,
+//! )?;
+//! 
+//! // Define equation of state.
+//! let saft = Rc::new(PcSaft::new(Rc::new(parameters)));
+//! 
+//! // Define thermodynamic conditions.
+//! let critical_point = State::critical_point(&saft, None, None, Default::default())?;
+//! 
+//! // Compute properties.
+//! let p = critical_point.pressure(Contributions::Total);
+//! let t = critical_point.temperature;
+//! println!("Critical point: T={}, p={}.", t, p);
+//! # Ok::<(), EosError>(())
+//! ```
+
 #![warn(clippy::all)]
 #![allow(clippy::too_many_arguments)]
 
