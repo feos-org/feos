5ceb67a432c2cc4ae4cf299aea91370c1c045de5
Gernot Bauer <gernotbauer85@googlemail.com>
2024-02-26 11:27:36 +0100

    Removed unchanged notebooks, pub keyword in density_iteration and added docs for methods in IdealGas trait.

--- feos-core/src/equation_of_state/ideal_gas.rs
+++ feos-core/src/equation_of_state/ideal_gas.rs
@@ -5,15 +5,15 @@ use num_dual::DualNum;
 
 /// Ideal gas Helmholtz energy contribution.
 pub trait IdealGas: Components + Sync + Send {
+    /// Implementation of an ideal gas model in terms of the
+    /// logarithm of the cubic thermal de Broglie wavelength
+    /// in units ln(A³) for each component in the system.
     fn ln_lambda3<D: DualNum<f64> + Copy>(&self, temperature: D) -> Array1<D>;
 
+    /// Short description (usually the name) of the model.
     fn ideal_gas_model(&self) -> String;
 
     /// Evaluate the ideal gas Helmholtz energy contribution for a given state.
-    ///
-    /// In some cases it could be advantageous to overwrite this
-    /// implementation instead of implementing the de Broglie
-    /// wavelength.
     fn ideal_gas_helmholtz_energy<D: DualNum<f64> + Copy>(&self, state: &StateHD<D>) -> D {
         let ln_lambda3 = self.ln_lambda3(state.temperature);
         ((ln_lambda3
