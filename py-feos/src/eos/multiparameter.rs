04e3b74b7d50ca58dec4222883608392ee5d65d1
Philipp Rehner <prehner@ethz.ch>
2025-10-17 15:35:00 +0200

    enable using the multiparameter ideal gas models independently

--- py-feos/src/eos/multiparameter.rs
+++ py-feos/src/eos/multiparameter.rs
@@ -19,7 +19,6 @@ impl PyEquationOfState {
     /// Returns
     /// -------
     /// EquationOfState
-    ///     The equation of state that can be used to compute thermodynamic states.
     #[staticmethod]
     pub fn multiparameter(parameters: PyParameters) -> PyResult<Self> {
         let eos = MultiParameter::new(parameters.try_convert()?);
@@ -30,5 +29,29 @@ impl PyEquationOfState {
             .map(IdealGasModel::MultiParameter)
             .collect();
         Ok(Self(Arc::new(EquationOfState::new(ideal_gas, residual))))
+    }
+
+    /// Ideal gas model used in multiparameter Helmholtz energy equations of state.
+    ///
+    /// Parameters
+    /// ----------
+    /// parameters : Parameters
+    ///     The parameters of the multiparameter Helmholtz equation of state.
+    ///
+    /// Returns
+    /// -------
+    /// EquationOfState
+    pub fn multiparameter_ideal_gas(
+        slf: Bound<'_, Self>,
+        parameters: PyParameters,
+    ) -> PyResult<Bound<'_, Self>> {
+        let eos = MultiParameter::new(parameters.try_convert()?);
+        let ideal_gas = eos
+            .ideal_gas
+            .into_iter()
+            .map(IdealGasModel::MultiParameter)
+            .collect();
+        slf.borrow_mut().add_ideal_gas(ideal_gas);
+        Ok(slf)
     }
 }
